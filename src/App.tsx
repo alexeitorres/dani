87950d2d66c30241b562b6cd723d4e791b1b5505
Alexei Torres <>
2026-06-10 23:08:35 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import BackgroundBirds from "./BackgroundBirds";
+
+function App() {
+  return (
+    <div id="maincontainer">
+      <BackgroundBirds />
+      <div id="text">
+        <p>Feliz Cumpleaños, Dani!</p>
+        <p id="sign">Alexei</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BackgroundBirds.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import BIRDS from "vanta/dist/vanta.birds.min";
+
+export default function BackgroundBirds() {
+  const vantaRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let effect = null;
+
+    if (vantaRef.current) {
+      effect = BIRDS.default({
+        el: vantaRef.current,
+        THREE,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200,
+        minWidth: 200,
+        backgroundColor: "#000000",
+        color1: "#8A1515",
+        color2: "#BF1BBC",
+        birdSize: 0.8,
+        wingSpan: 40,
+        speedLimit: 4.0,
+        separation: 50,
+        alignment: 50,
+        cohesion: 50,
+      });
+    }
+
+    return () => {
+      if (effect) effect.destroy();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={vantaRef}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: -1,
+      }}
+    />
+  );
+}
